f5f94f726a4aba619daf1c6c5bb7e75d07f6d818
Shweta7709 <shwetannashte7709@gmail.com>
2025-03-11 14:32:17 +0530

    code to Get a Character at a Specific Index

---
+++ CharAtExample.java
@@ -0,0 +1,6 @@
+public class CharAtExample {
+    public static void main(String[] args) {
+        String s1 = "Welcome To New Project";
+        System.out.println("Character at index 3: " + s1.charAt(3));
+    }
+}
